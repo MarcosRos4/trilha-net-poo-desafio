12d25a1d97cc288ba1dba91f808951c0f1a0545d
Marcos Vinicius Cavalcante Rosa <marcosviniciuscrosa@gmail.com>
2024-09-13 11:49:28 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("SmartPhone Nokia: ");
+Nokia nokia = new("40028922", "N+", "IMEI DO NOKIA", 300);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tetris 98");
+
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.WriteLine("\nSmartPhone Iphone: ");
+Iphone iphone = new("12345678", "Iphone 19", "IMEI DO IPHONE", 10000);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Crypto Wallet");
+Console.ResetColor();
